1495afd7a792afcf15af6c92704a395af2124155
Rishabh <baidrishabh443@gmail.com>
2020-10-05 15:57:21 +0530

    Updates

---
+++ precache-manifest.7c5634d3f9bbfbb6e9353c37b24458d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b993f2641b215eb23a0148df09556d73",
+    "url": "/techplacita/index.html"
+  },
+  {
+    "revision": "47ae674c05675eed75b1",
+    "url": "/techplacita/static/css/main.72082b22.chunk.css"
+  },
+  {
+    "revision": "ee5f07a6aa7da3484b6d",
+    "url": "/techplacita/static/js/2.d2027444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techplacita/static/js/2.d2027444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ae674c05675eed75b1",
+    "url": "/techplacita/static/js/main.f42b6c4f.chunk.js"
+  },
+  {
+    "revision": "edf7226390f728d05a46",
+    "url": "/techplacita/static/js/runtime-main.f429f023.js"
+  }
+]);
